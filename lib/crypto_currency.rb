e7e00a04c78dc90a6f9b430e3a29321b40521f81
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-07-09 15:28:12 -0400

    add class variable @@all to CryptoCurrency class to track instances of crypto currencies for list display

--- lib/crypto_currency.rb
+++ lib/crypto_currency.rb
@@ -2,9 +2,11 @@ require_relative '../config/environment'
 
 class CryptoCurrency
 	attr_accessor :name, :price, :market_cap, :circulating_supply, :volume, :percent_change
+	@@all = []
 
 	def initialize(data)
 		data.each{|key,value| self.send("#{key}=",value)}
+		@@all << self if !@@all.include?(self)
 	end	
 
 end
