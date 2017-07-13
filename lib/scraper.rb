4a721c5a6a3441bdc873b245ee48e0e55997bdcb
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-07-13 12:39:26 -0400

    condense sandwich code in Scraper's #get_list method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,19 +8,17 @@ class Scraper
 	end
 
 	def get_list
-		list = []
-		get_page.css("td.currency-name a").each {|crypto| list << crypto.text.downcase.gsub(" ","-") if list.size < 10 }
-		list
+		get_page.css("td.currency-name a" ).enum_for(:each_with_index).collect {|crypto, index| crypto.text.downcase.gsub(" ","-") if index < 10 }
 	end
 
 	def get_attributes(crypto_currency)
 		data = {}
-		data["name"] = crypto_currency
+		data["name"] = crypto_currency 
 		data["price"] = @page.css("tr#id-#{crypto_currency} td.no-wrap a.price").text
 		data["market_cap"] = @page.css("tr#id-#{crypto_currency} td.market-cap").text.gsub(/\s+/, "")
 		data["circulating_supply"] = @page.css("tr#id-#{crypto_currency} td.no-wrap a[target='_blank']").text.gsub(/\s+/, " ")
 		data["percent_change"] = @page.css("tr#id-#{crypto_currency} td.percent-24h").text
 		data
-	end
+	end 
 
 end
